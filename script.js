09b59e2a8f87f807d9ece58d7577b78e9f41e635
Am706870 <88397078+Am706870@users.noreply.github.com>
2025-02-19 14:18:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let counter = 0;
+function incrementCounter(){
+	alert("Current counter value:", +counter);
+
+	counter++;
+	document.getElementByID("counter").innerText = counter;
+}
